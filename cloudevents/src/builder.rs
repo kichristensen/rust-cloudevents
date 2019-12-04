925b7d46ca80dfd42a69b3bf1d80e291c4319d2a
Linus Basig <linus@basig.me>
2019-12-04 09:28:16 +0100

    fix: add docs to multi version builder

--- cloudevents/src/builder.rs
+++ cloudevents/src/builder.rs
@@ -3,15 +3,32 @@ use crate::v1_0::CloudEventV1_0Builder;
 
 type DefaultCloudEventBuilder = CloudEventV1_0Builder;
 
+/// Create a new `CloudEvent` in the desired spec version.
+///
+/// # Example
+/// 
+/// ```
+/// use cloudevents::CloudEventBuilder;
+///
+/// let event= CloudEventBuilder::v1_0()
+///     .event_id("id")
+///     .source("http://www.google.com")
+///     .event_type("test type")
+///     .datacontenttype("application/json")
+///     .build();
+/// ```
 pub struct CloudEventBuilder;
 
 impl CloudEventBuilder {
+    /// Create a new `CloudEvent` in the current spec version
     pub fn default() -> DefaultCloudEventBuilder {
         DefaultCloudEventBuilder::default()
     }
+    /// Create a new `CloudEvent` according to spec version 0.2
     pub fn v0_2() -> CloudEventV0_2Builder {
         CloudEventV0_2Builder::default()
     }
+    /// Create a new `CloudEvent` according to spec version 1.0
     pub fn v1_0() -> CloudEventV1_0Builder {
         CloudEventV1_0Builder::default()
     }
